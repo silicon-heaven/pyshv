d11288e4692fbdd1ed7ea4ef1105565fa540e375
Karel Kočí <cynerd@email.cz>
2022-11-15 15:46:52 +0100

    Add setup.py and convert chainpack to module

---
+++ chainpack/__init__.py
@@ -0,0 +1,15 @@
+from . import chainpack, clientconnection, cpcontext, cpon, rpcclient, rpcmessage, rpcvalue
+from .rpcclient import RpcClient
+from .rpcmessage import RpcMessage
+
+__all__ = [
+    "chainpack",
+    "clientconnection",
+    "cpcontext",
+    "cpon",
+    "rpcclient",
+    "rpcmessage",
+    "rpcvalue",
+    "RpcClient",
+    "RpcMessage",
+]
