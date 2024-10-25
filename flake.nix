a3186aa747e087a2ce617546c80fa0e67e410b88
Karel Kočí <kkoci@elektroline.cz>
2024-10-25 14:26:39 +0200

    flake: rework to not use with
    This also reuses project name from pyproject.toml in the flake.nix and
    thus reduces amount of required modifications.
    
    The app specification was also removed. The meta.mainProgram is now used
    instead.

--- flake.nix
+++ flake.nix
@@ -5,83 +5,88 @@
     self,
     flake-utils,
     nixpkgs,
-  }:
-    with builtins;
-    with flake-utils.lib;
-    with nixpkgs.lib; let
-      pyproject = trivial.importTOML ./pyproject.toml;
-      src = builtins.path {
-        path = ./.;
-        filter = path: _: ! hasSuffix ".nix" path;
+  }: let
+    inherit (builtins) match;
+    inherit (flake-utils.lib) eachDefaultSystem filterPackages;
+    inherit (nixpkgs.lib) head foldl trivial hasSuffix attrValues getAttrs composeManyExtensions;
+
+    pyproject = trivial.importTOML ./pyproject.toml;
+    inherit (pyproject.project) name version;
+    src = builtins.path {
+      path = ./.;
+      filter = path: _: ! hasSuffix ".nix" path;
+    };
+
+    pypi2nix = list: pypkgs:
+      attrValues (getAttrs (map (n: let
+          pyname = head (match "([^ =<>;]*).*" n);
+          pymap = {
+          };
+        in
+          pymap."${pyname}" or pyname)
+        list)
+        pypkgs);
+    requires = pypi2nix pyproject.project.dependencies;
+    requires-test = pypi2nix pyproject.project.optional-dependencies.test;
+    requires-docs = pypi2nix pyproject.project.optional-dependencies.docs;
+
+    pypackage = {
+      buildPythonPackage,
+      pytestCheckHook,
+      pythonPackages,
+      setuptools,
+      sphinxHook,
+    }:
+      buildPythonPackage {
+        pname = pyproject.project.name;
+        inherit version src;
+        pyproject = true;
+        build-system = [setuptools];
+        outputs = ["out" "doc"];
+        propagatedBuildInputs = requires pythonPackages;
+        nativeBuildInputs = [sphinxHook] ++ requires-docs pythonPackages;
+        nativeCheckInputs = [pytestCheckHook] ++ requires-test pythonPackages;
+        meta.mainProgram = "foo";
+      };
+  in
+    {
+      overlays = {
+        pythonPackagesExtension = final: _: {
+          "${name}" = final.callPackage pypackage {};
+        };
+        noInherit = _: prev: {
+          pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [self.overlays.pythonPackagesExtension];
+        };
+        default = composeManyExtensions [self.overlays.noInherit];
+      };
+    }
+    // eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system}.extend self.overlays.default;
+    in {
+      packages.default = pkgs.python3Packages."${name}";
+      legacyPackages = pkgs;
+
+      devShells = filterPackages system {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            editorconfig-checker
+            statix
+            deadnix
+            gitlint
+            ruff
+            (python3.withPackages (p:
+              [p.build p.twine p.sphinx-autobuild p.mypy]
+              ++ foldl (prev: f: prev ++ f p) [] [
+                requires
+                requires-docs
+                requires-test
+              ]))
+          ];
+        };
       };
 
-      attrList = attr: list: attrValues (getAttrs list attr);
-      requires = p: attrList p pyproject.project.dependencies;
-      requires-test = p: attrList p pyproject.project.optional-dependencies.test;
-      requires-docs = p: attrList p pyproject.project.optional-dependencies.docs;
+      checks.default = self.packages.${system}.default;
 
-      template-python = {
-        buildPythonPackage,
-        pytestCheckHook,
-        pythonPackages,
-        setuptools,
-        sphinxHook,
-      }:
-        buildPythonPackage {
-          pname = pyproject.project.name;
-          inherit (pyproject.project) version;
-          inherit src;
-          pyproject = true;
-          build-system = [setuptools];
-          outputs = ["out" "doc"];
-          propagatedBuildInputs = requires pythonPackages;
-          nativeBuildInputs = [sphinxHook] ++ requires-docs pythonPackages;
-          nativeCheckInputs = [pytestCheckHook] ++ requires-test pythonPackages;
-        };
-    in
-      {
-        overlays = {
-          pythonPackagesExtension = final: _: {
-            template-python = final.callPackage template-python {};
-          };
-          noInherit = _: prev: {
-            pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [self.overlays.pythonPackagesExtension];
-          };
-          default = composeManyExtensions [self.overlays.noInherit];
-        };
-      }
-      // eachDefaultSystem (system: let
-        pkgs = nixpkgs.legacyPackages.${system}.extend self.overlays.default;
-      in {
-        packages.default = pkgs.python3Packages.template-python;
-        legacyPackages = pkgs;
-
-        devShells = filterPackages system {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              editorconfig-checker
-              statix
-              deadnix
-              gitlint
-              ruff
-              (python3.withPackages (p:
-                [p.build p.twine p.sphinx-autobuild p.mypy]
-                ++ foldl (prev: f: prev ++ f p) [] [
-                  requires
-                  requires-docs
-                  requires-test
-                ]))
-            ];
-          };
-        };
-
-        apps.default = {
-          type = "app";
-          program = "${self.packages.${system}.default}/bin/foo";
-        };
-
-        checks.default = self.packages.${system}.default;
-
-        formatter = pkgs.alejandra;
-      });
+      formatter = pkgs.alejandra;
+    });
 }
