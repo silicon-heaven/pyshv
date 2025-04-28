1a7d0cbf6f25be6e9bc81b2184a68779ca64b666
Karel Kočí <kkoci@elektroline.cz>
2025-04-28 14:02:34 +0200

    rpctransport: remove import of rpctransport from root package
    This removes the rpctransport imports from root. Use `shv.rpctransport`
    instead. This is to clean up the root a bit. In most use cases external
    users won't need to touch rpctransports thus it is not desirable to have
    it in the root.

--- tests/test_rpctransport.py
+++ tests/test_rpctransport.py
@@ -15,14 +15,14 @@ import threading
 
 import pytest
 
-from shv import (
+from shv import RpcMessage
+from shv.rpctransport import (
     RpcClient,
     RpcClientPipe,
     RpcClientTCP,
     RpcClientTTY,
     RpcClientUnix,
     RpcClientWebSockets,
-    RpcMessage,
     RpcServerTCP,
     RpcServerTTY,
     RpcServerUnix,
